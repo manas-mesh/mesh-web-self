0b606724dc4ace004325a80ce8212f1d0c96eae0
Manas Gupta <manas94gupta@gmail.com>
2022-05-19 07:20:31 +0530

    refactor input and textarea styles

--- src/uiToolkit/Textarea/Textarea.tsx
+++ src/uiToolkit/Textarea/Textarea.tsx
@@ -1,50 +1,12 @@
 // Libraries
-import styled from '@emotion/styled';
 import { useTheme } from '@emotion/react';
-import {
-  Textarea as ChakraTextarea,
-  InputGroup,
-  InputRightElement,
-  FormControl,
-  FormLabel,
-  FormHelperText,
-} from '@chakra-ui/react';
+import { InputGroup, InputRightElement, FormLabel } from '@chakra-ui/react';
 
 // Typography
 import { TextLabelSmall, TextBodySmall } from '@uiToolkit/Typography';
 
-const StyledChakraTextarea = styled(ChakraTextarea)`
-  border: ${(props) => `1px dashed ${props.theme.colors.border.bg60}`};
-  border-width: 0 0 1px 0;
-  font-size: ${(props) => props.theme.fonts.size.body.medium};
-  color: ${(props) => props.theme.colors.text.bg20};
-  padding: 0;
-  background: ${(props) => props.theme.colors.formFields.transparentBg};
-  outline: none;
-  width: ${(props) => (props.endIcon ? `calc(100% - 16px)` : `100%`)};
-  resize: none;
-`;
-
-const StyledFormControl = styled(FormControl)`
-  background: ${(props) =>
-    props.withBackground ? props.theme.colors.formFields.bg : props.theme.colors.formFields.transparentBg};
-  padding: 12px;
-  border-radius: 8px;
-
-  &:hover {
-    background: ${(props) =>
-      props.withBackground ? props.theme.colors.formFields.hoverBg : props.theme.colors.formFields.transparentBg};
-  }
-
-  &:focus-within {
-    background: ${(props) =>
-      props.withBackground ? props.theme.colors.formFields.focusBg : props.theme.colors.formFields.transparentBg};
-  }
-`;
-
-const StyledFormHelperText = styled(FormHelperText)`
-  text-align: right;
-`;
+// Styles
+import { StyledChakraTextarea, StyledFormControl, StyledFormHelperText } from './Textarea.styles';
 
 export type TextareaProps = {
   type: string;
