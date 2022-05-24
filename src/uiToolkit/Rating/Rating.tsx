d4542741dbd0caed3a83ce72484653c441fea742
Kerim Balic <balickerim@gmail.com>
2022-05-24 14:59:15 +0200

    changed background color

--- src/uiToolkit/Rating/Rating.tsx
+++ src/uiToolkit/Rating/Rating.tsx
@@ -1,6 +1,7 @@
-import { Box } from '@chakra-ui/react';
+import { Box, chakra } from '@chakra-ui/react';
 import { baseTheme } from '@themes/clients/baseTheme';
 import { BaseStarRating } from '@uiToolkit/BaseStarRating/BaseStarRating';
+import styled from '@emotion/styled';
 
 interface RatingProps {
   precision?: number;
@@ -13,9 +14,20 @@ interface RatingProps {
   enableValidation?: boolean;
   isRequired?: boolean;
 }
+const Wrapper = chakra(Box, {
+  baseStyle: {
+    background: baseTheme.colors.surfaces.bg92,
+    padding: '12px',
+    borderRadius: '8px',
+    width: 'fit-content',
+    '&:focus': {
+      background: baseTheme.colors.formFields.focusBg,
+    },
+  },
+});
 
 export const Rating = ({ ...props }: RatingProps) => (
-  <Box sx={{ background: baseTheme.colors.surfaces.bg92, padding: '12px', borderRadius: '8px', width: 'fit-content' }}>
+  <Wrapper>
     <BaseStarRating {...props} />
-  </Box>
+  </Wrapper>
 );
