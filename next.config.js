e3514802b71314da795fca981af579b8d47e6b56
Manas Gupta <manas94gupta@gmail.com>
2022-06-21 14:45:33 +0530

    update next config to ignore ts build errors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 
 module.exports = {
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ['media.mesh.ai'],
   },
